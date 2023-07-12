b5fdd9b1ec00df6f39d5a8da9d2e984259424062
Josh Knox <jknox@hitachisolutions.com>
2023-07-12 15:08:55 -0500

    unit tests around string helpers and fixed an issue with spaces/special chars

--- Console/GoofingAround/GoofingAround/StringHelpers/StringHelpers.cs
+++ Console/GoofingAround/GoofingAround/StringHelpers/StringHelpers.cs
@@ -5,24 +5,28 @@ public static class StringHelpers
     public static string ReverseCase(this string s)
     {
         char[] tempChars = new char[s.Length];
-        //new string array
         char[] charArray = s.ToCharArray();
-        for (int i = charArray.Length - 1; i >= 0; i--)
+        for (int i = 0; i < charArray.Length; i++)
         {
-            //check if char is a space 
-            if (!Char.IsLetter(charArray[i]))
-                continue;
-            if (Char.IsUpper(charArray[i]))
+            if (Char.IsLetter(charArray[i]))
             {
-                tempChars[i] = Char.ToLower(charArray[i]);
+                if (Char.IsUpper(charArray[i]))
+                {
+                    tempChars[i] = Char.ToLower(charArray[i]);
+                }
+                else if (Char.IsLower(charArray[i]))
+                {
+                    tempChars[i] = Char.ToUpper(charArray[i]);
+                }
             }
-            else if (Char.IsLower(charArray[i]))
+            else
             {
-                tempChars[i] = Char.ToUpper(charArray[i]);
+                tempChars[i] = charArray[i];
             }
         }
         return new string(tempChars);
     }
+
 
     //Reverse the order of a string
     public static string Reverse(this string s)
---
+++ Console/GoofingAround/GoofingAroundTests/StringHelpers/StringHelpersTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoofingAround.StringHelpers.Tests
+{
+    [TestClass()]
+    public class StringHelpersTests
+    {
+        [TestMethod]
+        public void ReverseCase_ShouldReverseCaseOfLetters()
+        {
+            // Arrange
+            string input = "Hello World";
+            string expected = "hELLO wORLD";
+
+            // Act
+            string result = input.ReverseCase();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Reverse_ShouldReverseString()
+        {
+            // Arrange
+            string input = "Hello World";
+            string expected = "dlroW olleH";
+
+            // Act
+            string result = input.Reverse();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ReverseCase_ShouldNotChangeSpecialCharacters()
+        {
+            // Arrange
+            string input = "Hello!@#$%^&*()_+";
+            string expected = "hELLO!@#$%^&*()_+";
+
+            // Act
+            string result = input.ReverseCase();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Reverse_ShouldReverseStringWithSpecialCharacters()
+        {
+            // Arrange
+            string input = "Hello!@#$%^&*()_+";
+            string expected = "+_)(*&^%$#@!olleH";
+
+            // Act
+            string result = input.Reverse();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ReverseCase_ShouldHandleEmptyString()
+        {
+            // Arrange
+            string input = "";
+            string expected = "";
+
+            // Act
+            string result = input.ReverseCase();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Reverse_ShouldHandleEmptyString()
+        {
+            // Arrange
+            string input = "";
+            string expected = "";
+
+            // Act
+            string result = input.Reverse();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
